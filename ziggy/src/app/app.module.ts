5cdbbc24d1db29a78ee3d7c37055bf980704ad1d
srikarslash <srikar.slash@gmail.com>
2018-04-11 16:08:41 -0400

    Add components, services, firebase library

--- ziggy/src/app/app.module.ts
+++ ziggy/src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { ChatroomComponent } from './components/chatroom/chatroom.component';
+import { ChatformComponent } from './components/chatform/chatform.component';
+import { MessageComponent } from './components/message/message.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserItemComponent } from './components/user-item/user-item.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
+    ChatroomComponent,
+    ChatformComponent,
+    MessageComponent,
+    LoginComponent,
+    RegisterComponent,
+    NavbarComponent,
+    UserListComponent,
+    UserItemComponent
   ],
   imports: [
     BrowserModule
---
+++ ziggy/src/app/components/chatform/chatform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chatform',
+  templateUrl: './chatform.component.html',
+  styleUrls: ['./chatform.component.css']
+})
+export class ChatformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
